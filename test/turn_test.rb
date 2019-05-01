0864deefd819185529d4a2b2df350f60841c8bc6
Sarah Tatro <statrox4@gmail.com>
2019-05-01 16:55:37 -0600

    Add two tests & setup method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,38 +4,47 @@ require './lib/turn'
 require './lib/card'
 
 class TurnTest < Minitest::Test
+  attr_reader :card, :turn
+  def setup
+
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    @turn = Turn.new("Juneau", @card)
+
+    @turn_2 = Turn.new("Birmingham", @card)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_it_takes_in_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_it_is_correct
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    turn = Turn.new("Juneau", card)
-
-    assert_equal true, turn.correct?
+    assert_equal true, @turn.correct?
   end
 
-  def test_it_takes_feedback
+  def test_it_is_incorrect
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    turn = Turn.new("Juneau", card)
+    assert_equal false, @turn_2.correct?
+  end
 
-    assert_equal "Correct!", turn.feedback 
+  def test_it_takes_correct_feedback
+    # skip
+
+    assert_equal "Correct!", @turn.feedback
+  end
+
+  def test_it_takes_incorrect_feedback
+    # skip
+
+    assert_equal "Incorrect.", @turn_2.feedback
   end
 end
