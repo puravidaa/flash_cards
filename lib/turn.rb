9865f99f7cde1959d4fc984cbbad90b0367e3adc
Sarah Tatro <statrox4@gmail.com>
2019-05-01 20:01:03 -0600

    Add empty turns array

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,24 +1,19 @@
 require './lib/card'
 
 class Turn
-  attr_accessor :string, :card, :guess
+  attr_reader :card, :guess
 
   def initialize(guess, card)
     @card = card
     @guess = guess
-    @correct = false
   end
 
   def correct?
-    if @guess == @card.answer
-      return true
-    else
-      return false
-    end
+    @guess == @card.answer
   end
 
   def feedback
-    if self.correct? == true
+    if self.correct?
       return "Correct!"
     else
       return "Incorrect."
