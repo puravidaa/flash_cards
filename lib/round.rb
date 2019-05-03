b6ebb7d92c8863778721fb001b074dbaaf981ba2
Sarah Tatro <statrox4@gmail.com>
2019-05-03 17:15:04 -0600

    Add count method

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,34 @@
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/turn'
+# require 'binding.pry'
 
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :current_card
 
   def initialize(deck)
-    @deck = deck 
+    @deck = deck
+    @turns = []
+    @current_card_number = 0
+    @current_card = @deck.cards[@current_card_number]
   end
+
+  def take_turn(guess)
+    turn_1 = Turn.new(guess, @current_card)
+    # store turn in @turns
+    @turns << turn_1
+    # move to next card
+    @current_card_number += 1
+    @current_card = @deck.cards[@current_card_number]
+    turn_1
+  end
+
+  def count
+    @turns.length 
+  end
+
+  # how many guesses are correct
+
+
 end
