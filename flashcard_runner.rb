394a939a91dfc2a5cdf45b10cf76bd9223e24fd3
Sarah Tatro <statrox4@gmail.com>
2019-05-04 12:14:44 -0600

    Update questions

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,13 +5,14 @@ require './lib/turn'
 
 # create some cards
 
-@card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+@card_1 = Card.new("The city of La Paz in Bolivia is safe from fire because of what?:
+?", "Too much altitude - Not enough air for burning", :Geography)
 
-@card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+@card_2 = Card.new("By law, 89what is banned in Japanese restaurants?", "Tipping", :STEM)
 
-@card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+@card_3 = Card.new("Where does sound travel faster; water or air?", "Water", :STEM)
 
-@card_4 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+@card_4 = Card.new("What does a manometer measure?", "The pressure of a closed system.", :STEM)
 
 # put your cards in a deck
 
